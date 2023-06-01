ce14ea634d296db55b3865ce578d003be1dbca38
lorenzo-dominici <lorenzo.dominici@stud.unifi.it>
2023-06-01 16:24:30 +0200

    internal resource location fixed

--- core/urls.py
+++ core/urls.py
@@ -10,16 +10,16 @@ urlpatterns = [
     path('roles/', list_roles, name='roles-list'),
     path('tasks/', list_tasks, name='tasks-list'),
 
-    path('<str:username>/', view_user, name='user-details'),
-    path('<str:username>/<str:project_name>/', view_project, name='project-details'),
-    path('<str:username>/<str:project_name>/<str:role_name>/', view_role, name='role-details'),
-    path('<str:username>/<str:project_name>/<str:task_name>/', view_task, name='task-details'),
-
     path('projects/new/', edit_project, name='project-new'),
     path('<str:username>/<str:project_name>/new-role/', edit_role, name='role-new'),
     path('<str:username>/<str:project_name>/new-task/', edit_task, name='task-new'),
 
     path('<str:username>/<str:project_name>/edit/', edit_project, name='project-edit'),
-    path('<str:username>/<str:project_name>/<str:role_name>/edit', edit_role, name='role-edit'),
-    path('<str:username>/<str:project_name>/<str:task_name>/edit/', edit_task, name='task-edit'),
+    path('<str:username>/<str:project_name>/roles/<str:role_name>/edit/', edit_role, name='role-edit'),
+    path('<str:username>/<str:project_name>/tasks/<str:task_name>/edit/', edit_task, name='task-edit'),
+
+    path('<str:username>/', view_user, name='user-details'),
+    path('<str:username>/<str:project_name>/', view_project, name='project-details'),
+    path('<str:username>/<str:project_name>/roles/<str:role_name>/', view_role, name='role-details'),
+    path('<str:username>/<str:project_name>/tasks/<str:task_name>/', view_task, name='task-details'),
 ]
--- core/views.py
+++ core/views.py
@@ -63,42 +63,48 @@ def view_task(request, task_name, username, project_name):
 
 @login_required
 def edit_project(request, username = None, project_name = None):
-    if request.user.username != username:
+    if username and request.user.username != username:
         raise PermissionDenied
     project = get_object_or_404(Project, name = project_name, owner = request.user) if username and project_name else None
-    form = ProjectForm(request.POST or None, instance = project, exclude = ['name'] if project else [])
+    form = ProjectForm(request.POST or None, instance = project)
+    if project:
+        form.fields.pop('name', None)
     if request.method == 'POST' and form.is_valid():
         project = form.save(commit=False)
         project.owner = request.user
         project.save()
-        return redirect('core:project_details', username = username, project_name = project_name)
-    return render(request, 'project_edit.html', {'form': form})
+        return redirect('core:project-details', username = username, project_name = project_name)
+    return render(request, 'core/project-edit.html', {'form': form})
 
 @login_required
 def edit_role(request, username = None, project_name = None, role_name = None):
-    if request.user.username != username:
+    if username and request.user.username != username:
         raise PermissionDenied
     project = get_object_or_404(Project, name = project_name, owner = request.user) if username and project_name else None
     role = get_object_or_404(Role, name = role_name, project = project) if project and role_name else None
-    form = RoleForm(request.POST or None, instance = role, exclude = ['name'] if role else [])
+    form = RoleForm(request.POST or None, instance = role)
+    if role:
+        form.fields.pop('name', None)
     if request.method == 'POST' and form.is_valid():
         role = form.save(commit=False)
         role.project = project
         role.save()
-        return redirect('core:role_details', username = username, project_name = project_name)
-    return render(request, 'role_edit.html', {'form': form})
+        return redirect('core:role-details', username = username, project_name = project_name)
+    return render(request, 'core/role-edit.html', {'form': form})
 
 @login_required
 def edit_task(request, username = None, project_name = None, task_name = None):
-    if request.user.username != username:
+    if username and request.user.username != username:
         raise PermissionDenied
     project = get_object_or_404(Project, name = project_name, owner = request.user) if username and project_name else None
     task = get_object_or_404(Task, name = task_name, project = project) if project and task_name else None
-    form = TaskForm(request.POST or None, instance = task, exclude = ['name'] if task else [], project = project)
+    form = TaskForm(request.POST or None, instance = task, project = project)
+    if task:
+        form.fields.pop('name', None)
     if request.method == 'POST' and form.is_valid():
         task = form.save(commit=False)
         task.project = project
         task.save()
         task.save_m2m()
-        return redirect('core:project_details', username = username, project_name = project_name)
-    return render(request, 'project_edit.html', {'form': form})
+        return redirect('core:project-details', username = username, project_name = project_name)
+    return render(request, 'core/project-edit.html', {'form': form})
